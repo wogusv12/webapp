77d1cc4f5a961c6347faecfc4d3c5b085897788b
김재현 <wogusv12@naver.com>
2019-08-30 15:35:47 +0900

    20190830 javascript

--- lib.js
+++ lib.js
@@ -1,3 +1,14 @@
 /*자바스크립트 파일*/
 function over(obj){obj.src="b.jpg";}
 function out(obj){obj.src="c.jpg";}
+function evalParseIntIsNaN(){
+    var res = evel("2*3+4*6");
+    document.write("eval(\"2*3+4*6\")는 " + res + "<br>");
+    var m = parseInt("32");
+    document.write("parseInt(\"32\")는 "+m+"<br>");
+    var n = parseInt("0x32");
+    document.write("parseInt(\"0x32\")는 "+n+"<br><br>");
+
+    n = parseInt("hello");
+    if(isNaN(n))document.write("hello는 숫자가아닙니다.");
+}
